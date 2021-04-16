6eef4fae1bdf09d3f6bdb9371d078fdcb31f860a
ellykhodaie <elahekhodaie@yahoo.com>
2021-04-16 21:45:02 +0430

    user request type function added to API gateway class

--- MicroServiceArch/MicroServiceArch/SElab4/display.py
+++ MicroServiceArch/MicroServiceArch/SElab4/display.py
@@ -7,3 +7,58 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 
 #TODO each user should be able to register, login, show and update their profiles
+
+class API_gateway(viewsets.ViewSet):
+
+    def user_request_type(self,request):
+        req_type = request.data["type"]
+
+
+        if req_type == "register":
+            try:
+                return self.register(request.data)
+            except:
+                #TODO  handle the more than 3 times req part
+
+        elif req_type == "login":
+            try:
+                return self.login(request.data)
+            except:
+                #TODO  handle the more than 3 times req part
+
+        elif req_type == "show":
+            try:
+                return self.show(request.data)
+            except:
+                #TODO  handle the more than 3 times req part
+
+        elif req_type == "update":
+            try:
+                return self.update(request.data)
+            except:
+                #TODO  handle the more than 3 times req part
+
+
+        else :
+            return HttpResponse('Bad Request', status=400)
+
+
+
+
+
+    def register(self,data):
+        #TODO
+    def login(self,data):
+        #TODO
+    def show(self,data):
+        #TODO
+    def update(self,data):
+        #TODO
+
+
+
+
+
+
+
+
