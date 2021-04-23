ea93c9cc5ac39d3be9478d9c26bf763d7c3798a6
Sina <stavakkoli1999@gmail.com>
2021-04-23 15:36:03 +0430

    book model added

--- MicroServiceArch/SElab4/app.py
+++ MicroServiceArch/SElab4/app.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class MicroServiceAppConfig(AppConfig):
+class MicroServiceArchConfig(AppConfig):
     name = 'MicroServiceArch'
--- MicroServiceArch/SElab4/models.py
+++ MicroServiceArch/SElab4/models.py
@@ -5,10 +5,18 @@ from django.db import models
 # the different users
 
 class User(models.Model):
-    username = models.CharField(max_length=256, unique=True)
-    password = models.CharField(max_length=256)
+    username = models.CharField(max_length=250, unique=True)
+    password = models.CharField(max_length=250)
     mobile = models.CharField(max_length=11)
     email = models.EmailField()
     isAdmin = models.BooleanField(default=False)
-    token = models.CharField(max_length=256, default="")
+    token = models.CharField(max_length=250, default="")
     token_time = models.DateTimeField(auto_now=True)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=250)
+    author = models.CharField(max_length=250)
+    publisher = models.CharField(max_length=250)
+    category = models.CharField(max_length=250)
+    _id = models.AutoField(primary_key=True)
